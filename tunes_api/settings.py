007611977072a24b639811050ec126d0f05ff45f
Devante Gandy <vante@Devantes-MacBook-Pro.local>
2021-06-09 14:29:47 -0400

    heroku fixed

--- tunes_api/settings.py
+++ tunes_api/settings.py
@@ -91,8 +91,11 @@ WSGI_APPLICATION = 'tunes_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tunes_api',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost'
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
