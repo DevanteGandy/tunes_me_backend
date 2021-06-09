b542ec52372529d7f0990874cec19cadf260f9a4
Devante Gandy <vante@Devantes-MacBook-Pro.local>
2021-06-09 15:11:17 -0400

    heroku fixed

--- tunes_api/settings.py
+++ tunes_api/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'tunes_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tunes_api',
         'USER': '',
         'PASSWORD': '',
